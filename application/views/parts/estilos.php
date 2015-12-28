10c9f1c10ac4e3569a8a66c30e0ff41c54892e21
Byron Peña <byronpenna@gmail.com>
2015-12-27 21:48:35 -0600

    finalizacion de jornada}

--- application/views/parts/estilos.php
+++ application/views/parts/estilos.php
@@ -1,3 +1,4 @@
 <!-- css -->
 	<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/frameworks/css/bootstrap/css/bootstrap.css") ?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/frameworks/css/font-awesome/css/font-awesome.css") ?> >
 	<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/css/Universales/style.css")  ?> >	
--- application/views/parts/menu.php
+++ application/views/parts/menu.php
@@ -1,13 +1,19 @@
 <div class="row marginNull header">
-	<div class="col-lg-4">
-		<button class="btn">Iniciar sesión</button>
-		<div class="input-group">
-		  <span class="input-group-addon">@</span>
-		  <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+	<div class="col-lg-4 vcenter">
+		<div class="col-lg-4 divBtnHead">
+			<button class="btn btnSq btnPi btnLogin">Iniciar sesión</button>	
+		</div>
+		<div class="col-lg-8 divInputSeach">
+			<div class="input-group">
+			  <span class="input-group-addon">
+			  	<i class="fa fa-search"></i>
+			  </span>
+			  <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+			</div>	
 		</div>
 	</div>
-	<div class="col-lg-4">
-		
+	<div class="col-lg-4 divImgLogo vcenter">
+		<img class='imgLogo' src=<?php echo base_url("Content/img/generales/LOGO1azul.png") ?> >
 	</div>
 	<div class="col-lg-4">
 		
