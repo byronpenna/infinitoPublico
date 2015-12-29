e5cb0886c316a34a8f515c4161c130f1270636d6
Byron Peña <byronpenna@gmail.com>
2015-12-28 21:23:34 -0600

    finalizacion de jornada

--- application/views/parts/estilos.php
+++ application/views/parts/estilos.php
@@ -1,6 +1,10 @@
 <!-- Fav ico --> 
 	<link rel="icon" href=<?php echo base_url("Content/img/generales/LOGO1azul.ico") ?> type="image/gif">
 <!-- css -->
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/frameworks/css/bootstrap/css/bootstrap.css") ?>>
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/frameworks/css/font-awesome/css/font-awesome.css") ?> >
-	<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/css/Universales/style.css")  ?> >	
+	<!-- frameworks -->
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/frameworks/css/bootstrap/css/bootstrap.css") ?>>
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/frameworks/css/font-awesome/css/font-awesome.css") ?> >
+	<!-- estilos -->
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/css/Universales/style.css")  ?> >	
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("Content/css/Universales/media.css")  ?> >	
+		
--- application/views/parts/menu.php
+++ application/views/parts/menu.php
@@ -1,42 +1,44 @@
-<div class="divLogoOculto">
-	<a href=<?php echo site_url("Welcome") ?> >
-		<img class='imgLogo' src=<?php echo base_url("Content/img/generales/LOGO1azul.png") ?> >
-	</a>
-</div>
 <div class="row marginNull header">
-	<div class="col-lg-5 col-md-5 vcenter">
-		<div class="col-lg-4 divBtnHead">
-			<button class="btn btnSq btnPi btnLogin ">Iniciar sesión</button>	
-		</div>
-		<div class="col-lg-8 divInputSeach">
-			<div class="input-group">
-			  <span class="input-group-addon">
-			  	<i class="fa fa-search"></i>
-			  </span>
-			  <input type="text" class="form-control" placeholder="Noticias" aria-describedby="basic-addon1">
-			</div>	
-		</div>
-	</div><!--
-    --><div class="col-lg-2 col-md-2 divImgLogo vcenter ">
+	<div class="row marginNull divLogoOculto">
 		<a href=<?php echo site_url("Welcome") ?> >
 			<img class='imgLogo' src=<?php echo base_url("Content/img/generales/LOGO1azul.png") ?> >
 		</a>
-	</div><!--
-    --><div class="col-lg-5 col-md-5 vcenter">
-		<div class="row marginNull">
-			<nav class='navMenu'>
-				<ul>
-					<li>
-						<a href=<?php echo site_url("Conocenos") ?> >Conocenos</a>
-					</li>
-					<li>
-						<a href="#">Articulos</a>
-					</li>
-					<li>
-						<a href="#">Contactanos</a>
-					</li>
-				</ul>
-			</nav>
-		</div>
+	</div>
+	<div class="row marginNull">
+		<div class="col-lg-5 col-md-5 vcenter">
+			<div class="col-lg-4 divBtnHead">
+				<button class="btn btnSq btnPi btnLogin ">Iniciar sesión</button>	
+			</div>
+			<div class="col-lg-8 divInputSeach">
+				<div class="input-group">
+				  <span class="input-group-addon">
+				  	<i class="fa fa-search"></i>
+				  </span>
+				  <input type="text" class="form-control" placeholder="Noticias" aria-describedby="basic-addon1">
+				</div>	
+			</div>
+		</div><!--
+	    --><div class="col-lg-2 col-md-2 divImgLogo vcenter ">
+			<a href=<?php echo site_url("Welcome") ?> >
+				<img class='imgLogo' src=<?php echo base_url("Content/img/generales/LOGO1azul.png") ?> >
+			</a>
+		</div><!--
+	    --><div class="col-lg-5 col-md-5 vcenter">
+			<div class="row marginNull">
+				<nav class='navMenu'>
+					<ul>
+						<li>
+							<a href=<?php echo site_url("Conocenos") ?> >Conocenos</a>
+						</li>
+						<li>
+							<a href="#">Articulos</a>
+						</li>
+						<li>
+							<a href="#">Contactanos</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>	
 	</div>
 </div>
